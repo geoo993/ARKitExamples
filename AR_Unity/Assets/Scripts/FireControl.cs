4950a170a9edf42b75e0fd17d2999277990218ca
GEORGE QUENTIN <ggeoo93@gmail.com>
2018-02-06 13:09:14 +0000

    Add AR Scale and Rotation example.

---
+++ AR_Unity/Assets/Scripts/FireControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireControl : MonoBehaviour {
+
+    private ParticleSystem ps {
+        get {
+            return GetComponent<ParticleSystem>();
+        }
+    }
+    
+	public void StartFire () {
+		ps.Play();
+	}
+    
+    public void StopFire () {
+        ps.Stop();
+    }
+}
