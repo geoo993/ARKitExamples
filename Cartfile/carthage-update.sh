384b50c49d96c4532534756a863f0f9b533d7d82
GEORGE QUENTIN <ggeoo93@gmail.com>
2018-03-30 10:20:02 +0100

    updated carthage update file

--- Cartfile/carthage-update.sh
+++ Cartfile/carthage-update.sh
@@ -9,4 +9,4 @@
 # Set git to use the osxkeychain credential helper
 git config --global credential.helper osxkeychain
 
-carthage update $@ --use-submodules --no-build --no-use-binaries --platform iOS
+carthage update $@ --no-build --no-use-binaries --platform iOS
