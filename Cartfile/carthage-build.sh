a80a7e1db2f88f32b81c5bbda70f8ab8834ea492
GEORGE QUENTIN <ggeoo93@gmail.com>
2018-03-30 10:26:43 +0100

    removed builds

--- Cartfile/carthage-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#  carthage.sh
-#  StorySmarties
-#
-#  Created by Daniel Asher on 29/06/2016.
-#  Copyright © 2016 LEXI LABS. All rights reserved.
-
-carthage build --platform "iOS" $@ 
